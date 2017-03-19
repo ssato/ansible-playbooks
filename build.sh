0b1f9fb57140388e3aa67ee703908521998a111d
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-19 11:32:15 +0900

    enhancement: add rpm packaging stuff

---
+++ build.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# USAGE: [_SNAPSHOT_BUILD=1] ./build.sh
+#
+set -e
+
+source pkginfo.sh
+snapshot_build=${_SNAPSHOT_BUILD:-0}
+
+if test "x$snapshot_build" = "x1"; then
+    VERSION="${VERSION:?}.$(date +%Y%m%d)"
+fi
+
+echo "[Info] Preparing confgiure.ac for ${PACKAGE:?}-${VERSION:?} ..."
+sed "
+s/@PACKAGE@/${PACKAGE}/g
+s/@VERSION@/${VERSION}/g
+" configure.ac.in > configure.ac
+
+echo "[Info] Start to build SRPM ..."
+autoreconf -vfi && ./configure && make srpm
+
+# vim:sw=4:ts=4:et:
---
+++ pkginfo.sh
@@ -0,0 +1,3 @@
+PACKAGE=ansible-roles-ssato-data
+VERSION=0.1.0
+_SNAPSHOT_BUILD=1
